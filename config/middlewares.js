43b252e8dc8576af3b40f58d3600d2dc295724b0
arsaizdihar <arsadihar@gmail.com>
2022-08-15 19:04:45 +0700

    Fix image security policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,39 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::errors",
+  env.bool("UPLOAD_S3", false)
+    ? {
+        name: "strapi::security",
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              "connect-src": ["'self'", "https:"],
+              "img-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "dl.airtable.com",
+                env("S3_BUCKET") + ".is3.cloudhost.id",
+              ],
+              "media-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "dl.airtable.com",
+                env("S3_BUCKET") + ".is3.cloudhost.id",
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      }
+    : "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
